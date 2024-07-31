306fb903bb386646cac91087e64946b40c1d2aba
Antonio A <antoniotraderxd@gmail.com>
2024-07-31 11:00:40 +0000

    ultimos cambios al envio de gmails

--- classes/Email.php
+++ classes/Email.php
@@ -17,31 +17,42 @@ class Email{
         $this->token = $token;
     }
     public function enviarConfirmacion(){
+        
         $email = new PHPMailer();
         $email->isSMTP();
         $email->Host = $_ENV['EMAIL_HOST'];
         $email->SMTPAuth = true;
         $email->Port = $_ENV['EMAIL_PORT'];
         $email->Username = $_ENV['EMAIL_USER'];
-        $email->Password = $_ENV['EMAIL_PASS'];
-
-
-        $email->setFrom('appsalon@gmail.com');
-        $email->addAddress($this->email);
+        $email->Password = $_ENV['EMAIL_PASSWORD'];
+        
+        // Configuración del protocolo de seguridad
+        if ($_ENV['EMAIL_PORT'] == 465) {
+            $email->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        } else {
+            $email->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        }
+        
+        $email->setFrom($this->email);
+        $email->addAddress($this->email,'Appsalon@gmail.com');
         $email->Subject = 'Confirma tu cuenta';
-
+        
         $email->isHTML(true);
         $email->CharSet = 'UTF-8';
-
-        $contenido = '<html> ';
-        $contenido.= "<p>Hola: ". $this->nombre ." Has creado una cuenta en nuestra pagina confirma presionando el siguiente enlace</p>";
-        $contenido.= "<p>Presiona aqui: <a href = '".$_ENV['APP_URL']."/confirmar-cuenta?token=".$this->token."'>Confimar Cuenta</a></p>";
-        $contenido.= "<p>Si tu no solistaste este registro solo ignoralo</p>";
+        
+        $contenido = '<html>';
+        $contenido .= "<p>Hola: " . $this->nombre . " Has creado una cuenta en nuestra página. Confirma presionando el siguiente enlace:</p>";
+        $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['APP_URL'] . "/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
+        $contenido .= "<p>Si tú no solicitaste este registro, solo ignóralo.</p>";
         $contenido .= '</html>';
-
+        
         $email->Body = $contenido;
-
+        
         $email->send();
+    
+        
+        
+       
     }
     public function enviarInstrucciones(){
 
@@ -51,11 +62,18 @@ class Email{
         $email->SMTPAuth = true;
         $email->Port = $_ENV['EMAIL_PORT'];
         $email->Username = $_ENV['EMAIL_USER'];
-        $email->Password = $_ENV['EMAIL_PASS'];
-
-        $email->setFrom('antoniopro69@gmail.com');
-        $email->addAddress('appsalon@app.com', 'Appsalon.com');
-        $email->Subject = 'Restablece tu password';
+        $email->Password = $_ENV['EMAIL_PASSWORD'];
+        
+        // Configuración del protocolo de seguridad
+        if ($_ENV['EMAIL_PORT'] == 465) {
+            $email->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        } else {
+            $email->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        }
+        
+        $email->setFrom($this->email);
+        $email->addAddress($this->email,'Appsalon@gmail.com');
+        $email->Subject = 'restablece tu password';
 
         $email->isHTML(true);
         $email->CharSet = 'UTF-8';
